7a461a81755e2e674ea792ef7e13eed9c9031971
JaeHyeonKim <120623320+FutureMaker0@users.noreply.github.com>
2023-10-28 18:46:28 +0900

    ✨ feat: Django&DRF 예시 코드 추가

--- 1. Django & DRF/DRF/mixins/views.py
+++ 1. Django & DRF/DRF/mixins/views.py
@@ -1,3 +1,4 @@
+# APIView
 class ModelsAPIView(APIView):
     def get(self, request):
         models = Model.objects.all()
@@ -17,3 +18,32 @@ class ModelAPIView(APIView):
         model = get_object_or_404(Model, id=pk)
         serializer = TaskDetailSerializer(model)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# mixins
+from rest_framework import generics
+from rest_framework import mixins
+
+class ModelsAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
+
+    # get method 처리 함수(전체 모델)
+    # mixins.ListModelMixin과 연결
+    def get(self, request, *args, **kwargs):       
+        return self.list(request, *args, **kwargs)
+
+    # post method 처리 함수(전체 모델)
+    # mixins.CreateModelMixin과 연결
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+
+class ModelAPIMixins(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
+
+    # get method 처리 함수(특정 모델)
+    # mixins.RetrieveModelMixin과 연결
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
