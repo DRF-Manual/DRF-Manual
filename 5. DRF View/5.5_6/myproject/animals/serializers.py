10290a040f354563d54c3dd776f83e1b47f82fff
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-10-30 19:33:09 +0900

    Delete 5. DRF View directory

--- 5. DRF View/5.5_6/myproject/animals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Animal
-
-class AnimalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Animal
-        fields = ['id', 'name', 'species', 'age']
--- 5. DRF View/5.7/myproject/myproject/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('myapp.urls')),
-    # 다른 URL 패턴들...
-]
