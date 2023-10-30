923c0081b3291a386ae894e92682fcc9cd088b75
BEOM NAMGOONG <tombeom@naver.com>
2023-10-30 19:55:12 +0900

    ♻️ refactor: 폴더명 수정

--- 5_7_View/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('animals/', views.AnimalList.as_view()),
-    path('animals/<int:pk>/', views.AnimalDetail.as_view()),
-]
